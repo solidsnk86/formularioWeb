090da14b8991a9d555119c2c7c589cd9fbe251f5
Gabriel Calcagni <93176365+solidsnk86@users.noreply.github.com>
2023-05-13 20:14:10 -0300

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -3,7 +3,7 @@ function mostrarAnuncio() {
   anuncio.innerHTML = `
   <div id="anuncio">
   <h5>¡Por formularios perzonalizados! Contáctame por éste medio:<br>
-  <a href="https://api.whatsapp.com/send?phone=5492604586538&text=Hola,%20necesito%20un%20formulario%20perzonalizado%20,%20mi%20nombre%20es...%20" target="_blank">
+  <a href="https://api.whatsapp.com/send?phone=5492665290020&text=Hola,%20necesito%20un%20formulario%20perzonalizado%20,%20mi%20nombre%20es...%20" target="_blank">
    <img src="img/whatsapp.png" alt="" width="14px"> 2665-290020</a>
   </a>
   </h5>
